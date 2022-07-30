eb89bbb7a0e69f3e43bdf9ad94d1121d7686113a
Sunija M Rasheed <sunija@keralauniversity.ac.in>
2022-07-30 14:41:21 +0530

    altered sys out

--- src/TestGitClass.java
+++ src/TestGitClass.java
@@ -2,9 +2,9 @@
 public class TestGitClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello 1");
+		System.out.println("hello 1$$");
 		System.out.println("hello 2");
-
+		System.out.println("hello 3");
 	}
 
 }
